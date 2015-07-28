7fe4c54c02814da0aefc79e7998c568e1b728cc8
eRTK <t-erdmann@web.de>
2015-07-28 12:50:52 +0200

    Update t3964.h

--- Z180/t3964.h
+++ Z180/t3964.h
@@ -3,8 +3,8 @@
 #define TZ 40  /* 200 msec Ueberwachungszeit zwischen den Datenbytes */
 #define TQ 110 /* 550 msec Ueberwachungszeit nach STX */
 
-/********** BEIKIRCH interne Festlegung auf 48 Bytes *********/
-#define MAX_BLOCK_LEN 48
+/********** interne Festlegung auf 50 Bytes *********/
+#define MAX_BLOCK_LEN 50
 
 extern byte ausgab_3964( byte * puffer, unsigned anz ); /* Senderoutine 3964 */
 extern byte einles_3964( byte * puffer, unsigned anz, byte U_zeit ); /* Empfangsroutine 3964 */
